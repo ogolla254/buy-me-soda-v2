ada0be243797aec889ceb314980789ddc9fc8edb
ogolla254 <danixogolla0@gmail.com>
2026-05-09 11:16:12 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = 3001;
+const PORT = 3002;
 
 // Initialize Prisma Client with error handling
 let prisma;
